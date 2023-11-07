e51931036f01938becf69f13bd07487fec45ea94
shayan <shayan.133331@gmail.com>
2023-11-07 08:08:46 +0330

    commit

---
+++ src/components/resource/StoreResources.vue
@@ -0,0 +1,35 @@
+<template >
+    <ul>
+        <resource-item v-for="res in resources" 
+            :key="res.id" 
+            :title="res.tittle"
+            :id="res.id"
+            :description="res.description"
+            :link="res.link"
+            >
+            {{res}}
+        </resource-item>
+    </ul>
+</template>
+
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+    components : {
+        ResourceItem,
+    },
+    inject : ['resources'],
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+  }
+</style>
